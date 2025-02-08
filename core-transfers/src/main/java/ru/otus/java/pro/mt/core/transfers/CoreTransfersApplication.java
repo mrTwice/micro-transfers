e0bb529266e8c909b2a9ffb717335de97b2b6fee
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-02-09 00:19:04 +0300

    metrics demo added

--- core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/CoreTransfersApplication.java
+++ core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/CoreTransfersApplication.java
@@ -5,19 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreTransfersApplication {
-	/*
-	Домашнее задание:
-	1. Изучить текущее состояние проекта
-	2. В dev/environment добавьте docker-compose с поднятием Kafka
-	3. После успешного исполнения перевода отправьте в кафку (в топик: "mt.transfers.status.info") сообщение вида:
-	  {
-	    "transferId": "..",
-	    "status": "EXECUTED"
-	  }
-	4. Сделайте  "мини-сервис" нотификаций, который вычитывет топик кафки из п. 4 и выводит в лог сообщение
-	"По переводу ${id} клиенту отправлена нотификация"
-	 */
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreTransfersApplication.class, args);
 	}
