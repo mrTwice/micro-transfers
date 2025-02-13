b125a28253a1df8fe95b36253a91a84823735945
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-02-13 15:59:51 +0300

    fix

--- core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/CoreTransfersApplication.java
+++ core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/CoreTransfersApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreTransfersApplication {
 	/*
 	Домашнее задание:
-	1. Создайте отдельный класс для хранение настроек RestClient (url, readTimeout, writeTimeout)
+	1. Создайте отдельный класс для хранение настроек RestClient (url, readTimeout, connectTimeout)
 	2. Создайте бин-фабрику, которая по объекту из п.1 позволяет построить объект типа RestClient
-	3. При создании RestClient добавьте настройку readTimeout/writeTimeout (пока при создании используется только url)
+	3. При создании RestClient добавьте настройку readTimeout/connectTimeout (пока при создании используется только url)
 	4. В RestClientsConfig создавайте RestClient через полученную фабрику
 	5. Покройте openapi описанием все контроллеры и дто, которые используете
 	 */
