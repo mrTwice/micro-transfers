71a3612e8e93089c8b76c625c394fba82fd4b52e
mrTwice <yampolskiy86@gmail.com>
2025-02-19 21:20:36 +0300

    add swagger descriptions

--- limits/src/main/java/ru/otus/java/pro/mt/limits/dtos/RemainingLimitDto.java
+++ limits/src/main/java/ru/otus/java/pro/mt/limits/dtos/RemainingLimitDto.java
@@ -6,9 +6,16 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Schema(description = "Доступный лимит")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RemainingLimitDto {
+
+    @Schema(
+            description = "Доступный лимит",
+            example = "100.00",
+            required = true
+    )
     private BigDecimal remainingLimit;
 }
